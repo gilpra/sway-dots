c2a86aee75ab96d7b6b0f2f190317d8a465ad3a6
Gilang Pradana <gilangarya272001@gmail.com>
2026-05-31 21:46:15 +0700

    feat(sway): add gsettings set theme script

---
+++ .config/sway/gsetting.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.desktop.interface gtk-theme Tokyonight-Dark
+gsettings set org.gnome.desktop.interface icon-theme Tela-circle
+gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+
