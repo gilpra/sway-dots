3f3b5cd4ae0cd1f7e09aad0c00380a44308cd06f
Gilang Pradana <gilangarya272001@gmail.com>
2026-07-16 19:32:54 +0700

    refactor(sway): modularize config into seperate file

--- .config/sway/gsetting.sh
+++ .config/sway/gsetting.sh
@@ -3,4 +3,3 @@ gsettings set org.gnome.desktop.interface icon-theme Tela-circle
 gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
 gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-
