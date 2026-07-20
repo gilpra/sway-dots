8b971856b61b59b25171d56edb077a9421299a0a
Gilang Pradana <gilangarya272001@gmail.com>
2026-07-20 22:11:44 +0700

    feat(colors): update to tokyo night variant

--- .config/sway/gsetting.sh
+++ .config/sway/gsetting.sh
@@ -1,4 +1,4 @@
-gsettings set org.gnome.desktop.interface gtk-theme Tokyonight-Dark
+gsettings set org.gnome.desktop.interface gtk-theme Tokyonight-Night
 gsettings set org.gnome.desktop.interface icon-theme Tela-circle
 gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
 gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu"
