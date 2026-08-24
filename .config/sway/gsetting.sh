60d253359b436c27062535a29ab9529dd2b93398
Gilang Pradana <gilangarya272001@gmail.com>
2026-08-24 19:54:25 +0700

    feat(fontconfig): add 0xProto Nerd Font as default

--- .config/sway/gsetting.sh
+++ .config/sway/gsetting.sh
@@ -3,3 +3,6 @@ gsettings set org.gnome.desktop.interface icon-theme Tela-circle
 gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
 gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface font-name '0xProto Nerd Font 11'
+gsettings set org.gnome.desktop.interface document-font-name '0xProto Nerd Font 12'
+gsettings set org.gnome.desktop.interface monospace-font-name '0xProto Nerd Font Mono 11'
