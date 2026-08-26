b358f45224fc1e990e0270c8d6c45ddb8ef1ba6b
Gilang Pradana <gilangarya272001@gmail.com>
2026-08-26 20:43:55 +0700

    refactor(shell): switch from fish to zsh

--- .config/fish/functions/y.fish
+++
@@ -1,9 +0,0 @@
-function y --description "Yazi with cwd integration" --wraps yazi
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    command yazi $argv --cwd-file="$tmp"
-    set cwd (cat -- "$tmp")
-    if test -n "$cwd" -a "$cwd" != "$PWD"
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
